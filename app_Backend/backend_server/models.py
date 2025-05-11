81b5fef526a3ee0c0022283dee7f51a4d246d9b3
djasmit <djsmit@deakin.edu.au>
2025-05-11 15:36:55 +1000

    Workout Endpoint Tidying + Misc
    SUMMARY:
    Makes a few more changes to the backend to improve the database design. Workout and Message models now generate their unique UUIDs instead of relying on the front end to do so.
    Additionally, workout endpoints have been updated with exception handling and failstate HTTP responses. The login endpoint now returns (a 404 NOT FOUND) instead of revealing the email as correct
    Added a few misc improvements to the backend too.
    
    AFFECTED FILES:
    Frontend:
    app/lib/pages/set_workout_page.dart (workout creation page):
    - disabled the UUID generation
    - now captures new_session_id from response from endpoint
    
    Backend:
    App_Backend/backend_server/settings.py:
    - Django Settings Debug now gets value from .env to unify them
    
    App_Backend/backend_server/models.py (data models):
    SetWorkout Model:
    - session_ID now generated on backend, but can be set by frontend in DEBUG mode for testing
    
    HelpCenterMessage Model:
    - thread_ID now generated on backend, but can be set by frontend in DEBUG mode for testing
    
    App_Backend/backend_server/views.py (endpoints):
    - Added a GetDebugMode() function to check the debug toggle instead of manually checking in endpoints
    
    Login Endpoint:
    - now returns 404 not found when given an incorrect password, as with an incorrect email
    
    SetWorkout Endpoint:
    - invalid email now returns 404 not found
    - added exception handling
    
    WorkoutData Endpoint:
    - invalid session ID now triggers 404 (Not Found) instead of 400 (bad request)
    - added exception handling

--- app_Backend/backend_server/models.py
+++ app_Backend/backend_server/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import uuid
 
 
 # When creating a user you need to provide all 3 fields, out of which:
@@ -96,7 +97,7 @@ class HelpCentreMessage(models.Model):
     ]
 
     thread_number = models.UUIDField(max_length=100, unique=True,
-                                     default='')  # UUID format, generate in flutter and pass
+                                     default=uuid.uuid4)  # UUID format, generate in flutter and pass
     email = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     subject = models.CharField(max_length=50)
     topic = models.CharField(max_length=30, choices=TOPIC_CHOICES)
@@ -180,7 +181,7 @@ class WorkoutType(models.Model):
         (CONTINUOUS, 'Continuous'),
     ]
 
-    session_id = models.CharField(max_length=100, primary_key=True)
+    session_id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
     email = models.ForeignKey(MyUser, on_delete=models.CASCADE, default='',
                               to_field='email')  # when user deleted, delete records too
     name = models.CharField(max_length=20, choices=NAME_CHOICES)
